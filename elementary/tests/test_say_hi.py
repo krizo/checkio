f19ed2660ba000b4b6e1e4c2669583d40b30e68b
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-06-15 14:05:01 +0200

    delivering uni tests to the solution

---
+++ elementary/tests/test_say_hi.py
@@ -0,0 +1,7 @@
+import pytest
+from elementary.say_hi import say_hi
+
+
+def test_say_hi():
+    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
+    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
