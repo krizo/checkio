3efec78feac5c249f19debcf640005fdc820dde2
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-06-15 14:06:26 +0200

    solution for correct_sentence puzzle

---
+++ elementary/tests/test_correct_sentence.py
@@ -0,0 +1,8 @@
+import pytest
+from elementary.correct_sentence import correct_sentence
+
+def test_correct_sentence():
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
